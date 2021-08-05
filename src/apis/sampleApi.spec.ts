a0a2e0e803606b82365105cfb4e583f663709aae
yuki <yuki.onishi1129@gmail.com>
2021-08-05 15:24:48 +0900

    add: sample api test

--- src/apis/sampleApi.spec.ts
+++ src/apis/sampleApi.spec.ts
@@ -19,13 +19,35 @@ describe('【APIテスト】sampleApi test', () => {
         deleteFlg: false,
       }
     })
-    test('【正常系】処理が正常に動作する', async () => {
+    test('【正常系】処理が正常に動作する。', async () => {
       // 関数をmock化
       const apiMock = jest.fn(getSampleData)
       // mock関数の戻り値を設定する
       const apiMockFUnc = apiMock.mockReturnValue(Promise.resolve(sampleState))
 
       expect(await apiMockFUnc()).toEqual(sampleState)
+    })
+
+    test('【異常系】処理が失敗する。apiの戻り値がundefinedの場合', async () => {
+      // 関数をmock化
+      const apiMock = jest.fn(getSampleData)
+      // mock関数の戻り値を設定する (undefined)
+      const apiMockFUnc = apiMock.mockReturnValue(Promise.resolve(undefined))
+
+      expect(await apiMockFUnc()).toEqual(undefined)
+    })
+
+    test('【異常系】処理が失敗する。例外処理が発生した場合', async () => {
+      // 関数をmock化
+      const apiMock = jest.fn(getSampleData)
+      // mock関数を敢えて失敗させるよう設定する
+      const apiMockFUnc = apiMock.mockReturnValue(Promise.reject('error'))
+
+      try {
+        await apiMockFUnc()
+      } catch (error) {
+        expect(error).toMatch('error')
+      }
     })
   })
 })
